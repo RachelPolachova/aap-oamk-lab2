7a1203602b20e58e8cd38675bc651df54b85a2c9
xpolach4 <rachel.polachova@gmail.com>
2019-04-24 19:14:58 +0300

    Main activity fix

--- app/src/main/java/com/rachel/polachova/aap_oamk_lab2/MainActivity.java
+++ app/src/main/java/com/rachel/polachova/aap_oamk_lab2/MainActivity.java
@@ -2,12 +2,33 @@ package com.rachel.polachova.aap_oamk_lab2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setupUI();
+    }
+
+    private void setupUI() {
+        setupGameButton();
+    }
+
+    private void setupGameButton() {
+        final Button gameButton = new Button(this);
+        gameButton.setText("I am the button");
+        setContentView(gameButton);
+        gameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter ++;
+                gameButton.setText("You have pressed button for " + counter + " times.");
+            }
+        });
     }
 }
